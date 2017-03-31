abdcc63081dd70b646843e1f4eb5ac55ac1a63d8
Kevin Zeng <zengfenfei@gmail.com>
2017-03-31 15:50:05 +0800

    Receive message

--- src/Glip.ts
+++ src/Glip.ts
@@ -1,9 +1,11 @@
 import RestClient from 'ringcentral-ts/RestClient';
+import Subscription from 'ringcentral-ts/Subscription';
 import PagingResult from 'ringcentral-ts/PagingResult';
 
 export default class Glip {
 
 	rest: RestClient;
+	subscription: Subscription;
 
 	constructor(rest: RestClient) {
 		this.rest = rest;
@@ -13,6 +15,33 @@ export default class Glip {
 		return this.rest.post('/glip/posts', {
 			groupId, text
 		}).then<any>(res => res.json());
+	}
+
+	/**
+	 * { uuid: '8416043423403629512-8160187281949800354',
+  event: '/restapi/v1.0/glip/posts',
+  timestamp: '2017-03-31T07:46:52.013Z',
+  subscriptionId: 'eaeffeb1-82e2-44db-9db7-946b932448f7',
+  body:
+   { id: '37912580',
+     groupId: '9199618',
+     type: 'TextMessage',
+     text: 'Test notifications',
+     creatorId: '170853004',
+     addedPersonIds: null,
+     creationTime: '2017-03-31T07:46:51.706Z',
+     lastModifiedTime: '2017-03-31T07:46:51.706Z',
+     eventType: 'PostAdded' } }
+	 * @param cb 
+	 */
+	async receiveMessage(cb) {
+		let { subscription } = this;
+		if (!subscription) {
+			subscription = new Subscription(this.rest);
+			await subscription.subscribe(['/account/~/extension/~/glip/posts']);
+			this.subscription = subscription;
+		}
+		subscription.onMessage(cb);
 	}
 
 	getGroups(opts?: { type, pageToken, recordCount }): Promise<PagingResult<Group>> {
--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,15 @@ main();
 async function main() {
 	try {
 		let glip = new Glip((await glipAuth).rest);
-		let groups = await glip.getGroups();
+		glip.receiveMessage(msg => {
+			console.log("Glip message received", msg);
+		});
+
+		/*let groups = await glip.getGroups();
 		for (let group of groups.records) {
 			let result = await glip.sendMessage(group.id, 'Test Message sent to group ' + group.id);
 			console.log('>>', result)
-		}
+		}*/
 	} catch (e) {
 		console.log(">>>", e)
 	}
