d6c47760aa61fec3add2aca8863d343f202d7e09
Kevin Zeng <zengfenfei@gmail.com>
2017-04-06 14:48:33 +0800

    Receive sms notification

--- src/sms.ts
+++ src/sms.ts
@@ -1,22 +1,63 @@
+import Subscription from 'ringcentral-ts/Subscription';
 import Glip, { GlipMessage } from './Glip';
 import { getRc } from './rc-oauth';
 
 export async function receiveSms(glip: Glip, msg: GlipMessage, aiResult) {
-	let rc = await getRc(msg.creatorId);
-	let sub = rc.createSubscription();
-	sub.onMessage(function (evt) {
-		console.log('subscription', evt.body);
-		let smsEvt = evt.body;
-		let smsNotification = `Sms received for ${smsEvt.to[0].name}(${smsEvt.to[0].phoneNumber}):\n\n${smsEvt.subject}`;
-		glip.sendMessage(msg.groupId, smsNotification);
-	});
-	try {
-		await sub.subscribe(['/account/~/extension/~/message-store/instant?type=SMS']);
-	} catch (e) {
-		glip.sendMessage(msg.groupId, 'Subscribe for sms failed, ' + e);
-		return;
+	let glipUserId = msg.creatorId;
+	let sub = smsSubscriptions[glipUserId];
+	if (!sub) {
+		let rc = await getRc(glipUserId);
+		sub = new SmsSubscriptionForGlip(rc.createSubscription(), glip);
+		smsSubscriptions[glipUserId] = sub;
 	}
-	glip.sendMessage(msg.groupId, 'Sms notification enabled for this group.');
+	try {
+		if (await sub.addGroup(msg.groupId)) {
+			glip.sendMessage(msg.groupId, 'Future sms of your RingCentral account will be sent here.');
+		} else {
+			glip.sendMessage(msg.groupId, 'Sms notification already enabled for this chat.');
+		}
+	} catch (e) {
+		glip.sendMessage(msg.groupId, 'Enable sms notification failed:' + e);
+	}
+}
+
+
+let smsSubscriptions: { [glipUserId: string]: SmsSubscriptionForGlip } = {};
+
+class SmsSubscriptionForGlip {
+	recipientGroups: string[] = [];
+	ownerGlipUserId: string;
+
+	subscription: Subscription;
+	glip: Glip;
+
+	constructor(sub: Subscription, glip: Glip) {
+		this.subscription = sub;
+		this.glip = glip;
+		sub.onMessage((evt) => {
+			let smsEvt = evt.body;
+			let smsNotification = `Sms received for ${smsEvt.to[0].name}(${smsEvt.to[0].phoneNumber}):\n\n${smsEvt.subject}`;
+			for (let groupId of this.recipientGroups) {
+				glip.sendMessage(groupId, smsNotification);
+			}
+		});
+	}
+
+	/**
+	 * Return true if added, false if existed
+	 * @param groupId 
+	 */
+	async addGroup(groupId: string) {
+		let idx = this.recipientGroups.indexOf(groupId);
+		if (idx > -1) {
+			return false;
+		}
+		this.recipientGroups.push(groupId);
+		if (this.recipientGroups.length === 1) {
+			await this.subscription.subscribe(['/account/~/extension/~/message-store/instant?type=SMS']);
+		}
+		return true;
+	}
 }
 
 /* Sample sms notification:
