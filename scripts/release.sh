b0d8fd4ba9e52ab4f242e81549be5974bf5efcfb
Kevin Zeng <zengfenfei@gmail.com>
2017-04-07 20:20:09 +0800

    Automatic merge

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,6 @@
 workingBranch=$(git rev-parse --abbrev-ref HEAD)
 echo Your branch:$workingBranch
 git checkout heroku
-git merge $workingBranch
+git merge $workingBranch --no-edit
 git push heroku heroku:master
 git checkout $workingBranch
