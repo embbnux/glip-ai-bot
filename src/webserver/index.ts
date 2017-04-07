3db473c74cd14c9a3241720ba72e6d2b04359ed2
Kevin Zeng <zengfenfei@gmail.com>
2017-04-07 21:03:36 +0800

    Check oauth call back url parameter

--- src/webserver/index.ts
+++ src/webserver/index.ts
@@ -4,8 +4,12 @@ let app: any = express();
 
 app.get('/rc-oauth-callback', async (req, res) => {
 	let fullUrl = req.protocol + '://' + req.headers.host + req.url;
+	let state: string = req.query.state;
+	if (!state || !state.match(/.+:.+/)) {
+		return res.end('Invalid state parameter.');
+	}
 	try {
-		await rcOauth.loggedIn(req.query.state, fullUrl);
+		await rcOauth.loggedIn(state, fullUrl);
 		res.setHeader('content-type', 'text/html');
 		res.end('<html><body>Login success, close me and go back to glip.<script>window.close();</script></body></html>');
 	} catch (e) {
